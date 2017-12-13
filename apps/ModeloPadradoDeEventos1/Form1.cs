e3fd76d3a3e3275261a3cd68797d613065c752e6
marcoaureliomunhoz <marcoaureliomunhoz@gmail.com>
2017-12-13 18:47:12 -0200

    ModeloPadraoDeEventos1

---
+++ apps/ModeloPadradoDeEventos1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModeloPadradoDeEventos1
+{
+    public partial class Form1 : Form
+    {
+        MeuBotao meuBotao;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            meuBotao = new MeuBotao();
+            meuBotao.Left = 20;
+            meuBotao.Top = 10;
+            meuBotao.Text = "Clique Aqui";
+            meuBotao.Width = 100;
+            meuBotao.Height = 30;
+            meuBotao.AlteracaoCor += btnCliqueAqui_AlteracaoCor;
+            this.Controls.Add(meuBotao);
+        }
+
+        /* método final */
+        private void btnCliqueAqui_AlteracaoCor(object sender, AlteracaoCorEventArgs e)
+        {
+            richTextBox1.AppendText(e.CorAnterior + " => " + meuBotao.BackColor.ToString() + "\n");
+        }
+    }
+}
---
+++ apps/ModeloPadradoDeEventos1/MeuBotao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModeloPadradoDeEventos1
+{
+    public class AlteracaoCorEventArgs : EventArgs
+    {
+        /* as propriedades públicas são acessíveis ao mundo externo */
+        public string CorAnterior { get; set; }
+
+        public AlteracaoCorEventArgs(string corAnterior)
+        {
+            CorAnterior = corAnterior;
+        }
+    }
+
+    public class MeuBotao : Button
+    {
+        //public
+        /* classe que faz a intermediação entre MeuBotao e o mundo externo */
+        public event EventHandler<AlteracaoCorEventArgs> AlteracaoCor;
+
+        //protected        
+        /* método emissor primário */
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+
+            int r = random.Next(0, 255);
+            int g = random.Next(0, 255);
+            int b = random.Next(0, 255);
+
+            string corAnterior = this.BackColor.ToString();
+
+            this.BackColor = Color.FromArgb(r, g, b);
+            this.ForeColor = Color.FromArgb(g, b, r);
+
+            OnAlteracaoCor(new AlteracaoCorEventArgs(corAnterior));
+        }
+
+        /* método emissor final */
+        protected virtual void OnAlteracaoCor(AlteracaoCorEventArgs e)
+        {
+            if (AlteracaoCor != null)
+            {
+                AlteracaoCor(this, e);
+            }
+        }
+
+        //private
+        private Random random = new Random();
+    }
+}
